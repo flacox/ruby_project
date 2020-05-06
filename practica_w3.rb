a8753ff049546677a292dba1dbd29c5a27843431
Francisco Taveras <anderson0430@gmail.com>
2020-05-06 13:58:09 -0700

    add a second way of mixing arrays

--- practica_w3.rb
+++ practica_w3.rb
@@ -1,6 +1,6 @@
 
 #Practicas con Arrays
-#num =[0,1,2,3,4,5,6,7,8,9]
+num =[0,1,2,3,4,5,6,7,8,9]
 
 #Obtener solo los pares
 # pares = num.select {|num| num.even?}
@@ -13,6 +13,9 @@
 #Combinarlo con su inverso
 # num_reversed = num.reverse
 # puts num.zip(num_reversed)
+
+#otra forma seria
+puts num.zip(num.reverse).flatten
 
 #Practicas con Hash o Diccionarios
 # num = { :one => 'uno', :two => 'dos', :three => 'tres' }
@@ -61,36 +64,52 @@
 # puts "El area de un triangulo con una altura de #{altura} y la base de #{base} es: #{c.triangulo}"
 
 #clase que Vector, que realiza: suma, resta, multiplicacion, division, y producto punto de 2 instancias de la clase
-class Calculadora
-    def initialize (num1, num2)
-        @num1 = num1
-        @num2 = num2
-    end
+# class Calculadora
+#     def initialize (num1, num2)
+#         @num1 = num1
+#         @num2 = num2
+#     end
 
-    def suma
-        @num1 + @num2
-    end
+#     def suma
+#         @num1 + @num2
+#     end
 
-    def resta
-        @num1 - @num2
-    end
+#     def resta
+#         @num1 - @num2
+#     end
 
-    def multiplica
-        @num1 * @num2
-    end
+#     def multiplica
+#         @num1 * @num2
+#     end
 
-    def divide
-        @num1 / @num2
-    end
+#     def divide
+#         @num1 / @num2
+#     end
 
-end
+# end
 
-num1 = 115
-num2 = 10
+# num1 = 115
+# num2 = 10
 
-c = Calculadora.new(num1, num2)
-puts c.suma
-puts c.resta
-puts c.multiplica
-puts c.divide
+# c = Calculadora.new(num1, num2)
+# puts c.suma
+# puts c.resta
+# puts c.multiplica
+# puts c.divide
 
+#crear una clases con un metodos de clase dado un arreglo de numeros enteros, 
+#retorna esa misma cantidad de figuras geometricas
+
+# class Cuadrado
+#     def self.create_all(longitudes)
+#       longitudes.map { |longitud_lado| self.new(longitud_lado) }
+#     end
+#     def initialize(longitud_lado)
+#       @longitud_lado = longitud_lado
+#     end
+#     def area
+#       @longitud_lado ** 2
+#     end
+#   end
+
+  
